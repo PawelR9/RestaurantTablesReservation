5cbdf8e469af0f4f383946efd3976a11af01bfc9
Paweł <pawel458it@gmail.com>
2023-09-05 13:26:52 +0200

    Initial commit

---
+++ src/main/java/pl/application/reservation/tables/restaurant/restControllers/HomeController.java
@@ -0,0 +1,12 @@
+package pl.application.reservation.tables.restaurant.restControllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/index")
+public class HomeController {
+
+
+}
